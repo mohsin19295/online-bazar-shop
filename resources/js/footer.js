da59086aff5bc05d40f3ddcccd6f3963ec0a018c
mohsin19295 <mohsin19295@gmail.com>
2022-07-04 12:21:39 +0530

    footer imported

---
+++ resources/js/footer.js
@@ -0,0 +1,45 @@
+function footer() {
+    return `<div class="row container-fluid mx-auto pt-5">
+            <div class="footer-box col-lg-3 col-md-6 col-12 mb-3">
+                <img id="footer-logo" src="./resources/images/logo.png" alt="">
+                <p class="mt-3">Online Shoping Bazar</p>
+
+            </div>
+
+            <div class="footer-box col-lg-3 col-md-6 col-12 mb-3">
+                <h4>CONNECT</h4>
+                <ul class="list-unstyled">
+                    <li class="mb-1"><a href="#">Our story</a></li>
+                    <li class="mb-1"><a href="#">Community</a></li>
+                    <li><a href="#">Commitment</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-box col-lg-3 col-md-6 col-12 mb-3">
+                <h4>CUSTOMER SUPPORT</h4>
+                <ul class="list-unstyled">
+                    <li class="mb-1"><a href="#">Privacy Policy</a></li>
+                    <li class="mb-1"><a href="#">FAQs</a></li>
+                    <li class="mb-1"><a href="#">Returns and Exchanges</a></li>
+                    <li class="mb-1"><a href="#">Cancellation Policy</a></li>
+                    <li class="mb-1"><a href="#">Shipping and Delivery</a></li>
+                    <li><a href="#">T&Cs</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-box col-lg-3 col-md-6 col-12 mb-3">
+                <h4 class="pb-2">FOLLOW US</h4>
+                <div class="row m-0 mb-5">
+                    <i class="fa-brands fa-facebook pr-3"></i>
+                    <i class="fa-brands fa-twitter pr-3"></i>
+                    <i class="fa-brands fa-linkedin pr-3"></i>
+                    <i class="fa-brands fa-instagram pr-3"></i>
+                    <i class="fa-brands fa-pinterest"></i>
+                </div>
+                <h4 class="pb-2">Write To Us</h4>
+                <p>support@thebadbilly.com</p>
+            </div>
+        </div>`;
+}
+
+export default footer
